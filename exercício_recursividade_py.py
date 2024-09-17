8af40b6e0204ba40caca25a8f47a7621ba72c1a4
EVANDRO DE OLIVEIRA <ra202412559@univassouras.edu.br>
2024-09-17 19:01:19 -0300

    Update exercício_recursividade_py.py

--- exercício_recursividade_py.py
+++ exercício_recursividade_py.py
@@ -14,11 +14,8 @@ print(potencia(2, 3))
 
 def fibonacci(n):
 
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-
+   if n < = 1:
+       return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
 
